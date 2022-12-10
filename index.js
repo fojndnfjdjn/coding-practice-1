5faa86a69e0d1f2e077d3634fc752796bf3c6562
fojndnfjdjn <119886430+fojndnfjdjn@users.noreply.github.com>
2022-12-11 05:12:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,71 +11,21 @@ const getData = async () => {
     const site_ = process.env.site_;
     let uniqueId = parseInt('0', 10);
 
-    await db.useServiceAccount({
-      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+    const browser = await puppeteer.launch({ headless: true });
+    const site_url = `${site_}${uniqueId}#`;
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+    await page.setDefaultTimeout(0);
+    await page.setExtraHTTPHeaders({
+      'user-agent':
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604.1',
     });
+    console.log('before goto');
+    await page.goto(site_url, { waitUntil: 'networkidle0' });
+    console.log('after goto');
 
-    // add a new table
-    const table = await db.addTable(`entries_${process.env.COUNT}`, [
-      'uniqueId',
-      'owner_name',
-      'ward_number',
-      'zone_number',
-      'locality',
-      'colony_data',
-      'mobile_number',
-      'email_address',
-    ]);
-
-    for (let index = 0; index < 900; index++) {
-      const browser = await puppeteer.launch({ headless: true });  
-      const site_url = `${site_}${uniqueId}#`;
-      const page = await browser.newPage();
-      await page.setDefaultNavigationTimeout(0);
-      await page.setDefaultTimeout(0);
-      await page.setExtraHTTPHeaders({
-        'user-agent':
-          'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604.1',
-      });
-console.log('before goto')
-      await page.goto(site_url, { waitUntil: 'networkidle0' });
-console.log('after goto')
-//       const htmlAfterLoadComplete = await page.evaluate(
-//         () => document.querySelector('#WD2E-r').outerHTML
-//       );
-
-//       const $ = cheerio.load(htmlAfterLoadComplete);
-//       const ownerName = $('#WD33').text();
-//       const wardNumber = $('#WD38').text();
-//       const zoneNumber = $('#WD3C').text();
-//       const localityData = $('#WD41').text();
-//       const colonyData = $('#WD46').text();
-//       const mobileNumber = $('#WD4B').val();
-//       const emailId = $('#WD50').val();
-
-      // Insert Single Entry
-//       await table.insertOne({
-//         uniqueId: uniqueId,
-//         owner_name: ownerName,
-//         ward_number: wardNumber,
-//         zone_number: zoneNumber,
-//         locality: localityData,
-//         colony_data: colonyData,
-//         mobile_number: mobileNumber,
-//         email_address: emailId,
-//       });
-
-      await page.close();
-      await browser.close();
-      uniqueId = uniqueId + 1;
-    }
-
-    uniqueId = uniqueId - 3;
-//     fs.writeFile('uuid.txt', '' + uniqueId, { flag: 'wx' }, function (err) {
-//       if (err) throw err;
-//       console.log("It's saved!");
-//     });
+    await page.close();
+    await browser.close();
 
     return true;
   } catch (error) {
